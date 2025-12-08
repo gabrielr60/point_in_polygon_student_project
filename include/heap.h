60c59cc0c8290ae1f269ebeb42e261f659c55245
eliasrw2003 <eliasrw2003@gmail.com>
2025-12-08 14:29:48 +0100

    Update heap.h

--- include/heap.h
+++ include/heap.h
@@ -3,21 +3,32 @@
 
 #include "point.h"
 
+/*
+ * Max-heap for storing the k closest points found so far in neigbours search.
+ * Stores distance to ceratin point and the pointindex.
+ * Ordering after distance.
+*/
+
+/* Element stored in the heap: distance and corresponding point index. */
 typedef struct {
-    double dist;   
-    Point *pt;     
+    double dist;    
+    int pointIndex;     
 } HeapElem;
 
+/* Max-heap structure. */
 typedef struct {
-    HeapElem *data;  
-    int size;        
-    int capacity;    
+    HeapElem *data;     // Array of heap elements
+    int size;           // Current number of elements in the heap
+    int capacity;       // Storage capacity
 } MaxHeap;
 
+/*Create a new heap with the specified capacity.*/
 MaxHeap *heap_create(int capacity);
 
-void heap_insert(MaxHeap *H, double dist, Point *pt);
+/*Insert a new element if heap not full or new element samller then max element.*/
+void heap_insert(MaxHeap *H, double dist, int newPointIndex);
 
+/*Free memory from heap.*/
 void heap_free(MaxHeap *H);
 
 #endif
