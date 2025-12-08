9fd04200a116b05050b5ed00d60f8e843be12f32
eliasrw2003 <eliasrw2003@gmail.com>
2025-12-08 14:35:36 +0100

    Update heap.c

--- src/heap.c
+++ src/heap.c
@@ -2,16 +2,19 @@
 #include "heap.h"
 #include <stdio.h>
 
+/* Reestablish max-heap property by pushing down element at index i.*/
 static void heapify_down(MaxHeap *H, int i) {
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
 
+    // Find largest among node and children (if within bounds)
     if (left < H->size && H->data[left].dist > H->data[largest].dist)
         largest = left;
     if (right < H->size && H->data[right].dist > H->data[largest].dist)
         largest = right;
 
+    // swap if child is larger and continue down the tree
     if (largest != i) {
         HeapElem tmp = H->data[i];
         H->data[i] = H->data[largest];
@@ -20,9 +23,12 @@ static void heapify_down(MaxHeap *H, int i) {
     }
 }
 
+/*Restore the max-heap property letting element at index "float up".*/
 static void heapify_up(MaxHeap *H, int i) {
-    if (i == 0) return;
+    if (i == 0) return;  // root reached
     int parent = (i - 1) / 2;
+
+    // If child is larger than parent, swap and continue upward
     if (H->data[i].dist > H->data[parent].dist) {
         HeapElem tmp = H->data[i];
         H->data[i] = H->data[parent];
@@ -31,30 +37,40 @@ static void heapify_up(MaxHeap *H, int i) {
     }
 }
 
+/* Allocate a new max-heap*/
 MaxHeap *heap_create(int capacity) {
     MaxHeap *H = malloc(sizeof(MaxHeap));
     if (!H) return NULL;
+
     H->data = malloc(sizeof(HeapElem) * capacity);
     if (!H->data) {
         free(H);
         return NULL;
     }
+
     H->size = 0;
     H->capacity = capacity;
     return H;
 }
 
-void heap_insert(MaxHeap *H, double dist, Point *pt) {
+/*
+ * Insert (dist, newPointIndex) into the heap.
+ * Only inserts if heap not full or samaller then max element. 
+*/
+void heap_insert(MaxHeap *H, double dist, int newPointIndex) {
     if (H->size < H->capacity) {
-        H->data[H->size] = (HeapElem){dist, pt};
+        //append and bubble up
+        H->data[H->size] = (HeapElem){dist, newPointIndex};
         heapify_up(H, H->size);
         H->size++;
     } else if (dist < H->data[0].dist) {
-        H->data[0] = (HeapElem){dist, pt};
+        // Replace maximum only if the new key is smaller
+        H->data[0] = (HeapElem){dist, newPointIndex};
         heapify_down(H, 0);
     }
 }
 
+/*Free all heap memory.*/
 void heap_free(MaxHeap *H) {
     if (!H) return;
     free(H->data);
