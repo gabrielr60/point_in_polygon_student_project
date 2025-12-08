e298d885d93e8152ee82c99e61bc67c0a6fd12f0
eliasrw2003 <eliasrw2003@gmail.com>
2025-12-08 14:34:41 +0100

    Update point.h

--- include/point.h
+++ include/point.h
@@ -1,15 +1,18 @@
 #ifndef POINT_H
 #define POINT_H
 
+/*Points in 3D storing x,y and z value*/
+
 typedef struct {
     double x;
     double y;
     double z;
 } Point;
 
-static inline float distSquared(Point* p1, Point* p2){
+/*returns the squared distance between two points (euclidian norm)*/
+static inline float distSquared(Point p1, Point p2){
 
-    return (p1->x-p2->x)*(p1->x-p2->x) + (p1->y-p2->y)*(p1->y-p2->y) + (p1->z-p2->z)*(p1->z-p2->z);
+    return (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) + (p1.z-p2.z)*(p1.z-p2.z);
 }
 
 #endif
